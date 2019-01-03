2ae605cc20c043ff89b3892d25a1ae3091c712f6
siddartha saif <saifulabu25@gmail.com>
2019-01-02 23:34:07 -0600

    414 inital commit

---
+++ src/SolvedProblems/Main_414.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SolvedProblems;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author sid
+ */
+public class Main_414 {
+
+    public static final boolean DEBUG = true;
+
+    public static void main(String[] args) throws IOException {
+        BufferedOutputStream output;
+        StringBuilder sb = new StringBuilder(2000); //assume initial size of 2000 chars
+        //InputReader input;
+        Scanner input;
+        if (DEBUG) {
+            output = new BufferedOutputStream(System.out);
+            input = new Scanner(new File("input.txt"));;
+        }else{
+            output = new BufferedOutputStream(System.out);
+            input = new Scanner(System.in);
+        }
+
+        String str;
+        while(input.hasNext()){
+            str = input.nextLine();
+            StringTokenizer stringTokenizer = new StringTokenizer(str);
+            System.out.println(str);
+            //int tapeCapacity = Integer.parseInt(stringTokenizer.nextToken());
+           // sb.append("\n");
+
+        }
+
+        output.write(sb.toString().getBytes());
+        output.flush();
+        output.close();
+
+    }// end of static void main
+
+    static class InputReader {
+
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public String readLine() {
+            int c = read();
+            //theres no ASCII -1 value. this signify EOF, and we return empty string
+            if (c == -1) {
+                return "";
+            }
+
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (c != '\n');
+            return res.toString();
+        }
+
+        public String readString() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public String next() {
+            return readString();
+        }
+
+        public interface SpaceCharFilter {
+
+            public boolean isSpaceChar(int ch);
+        }
+    }
+
+    static class OutputWriter {
+
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+        public void flush() {
+            writer.flush();
+        }
+    }
+
+    static class IOUtils {
+
+        public static int[] readIntArray(InputReader in, int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = in.readInt();
+            }
+            return array;
+        }
+    }
+}//End of Main Class
