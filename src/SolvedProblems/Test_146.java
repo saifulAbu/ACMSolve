0d00232339a0fc12b3a3da7d1180b34d966b17f9
siddartha saif <saifulabu25@gmail.com>
2019-03-21 08:47:03 -0500

    added all the methods

--- src/SolvedProblems/Test_146.java
+++ src/SolvedProblems/Test_146.java
@@ -11,12 +11,21 @@ package SolvedProblems;
  */
 public class Test_146 {
     public static void main(String args[]){
-        String str = "0125330";
+        String str = "cbbaa";
         char arr[] = str.toCharArray();
-        System.out.println(suffixLongestDecreasingSequence(arr));
+        int suffixIndex = suffixLongestDecreasingSequenceStartIndex(arr);
+        if(suffixIndex == 0){
+            System.out.println("no successor");
+            return;
+        }
+        int minPrefixIndex = minElemInSuffixIndex(suffixIndex, arr);
+        swap_min_suffix_last_elem_prefix(suffixIndex, minPrefixIndex, arr);
+        reverseSuffix(suffixIndex, arr);
+        String result = String.valueOf(arr);
+        System.out.println(result);
     }
     
-    static int suffixLongestDecreasingSequence(char[] input){
+    static int suffixLongestDecreasingSequenceStartIndex(char[] input){
         int size = input.length;
         int index = size -1;
         
@@ -27,5 +36,37 @@ public class Test_146 {
             index = i;
         }
         return index;
+    }
+    
+    static int minElemInSuffixIndex(int suffixStartIndex, char[] input){
+        int minSuffixIndex = suffixStartIndex;
+        int minElem = input[suffixStartIndex];
+        for(int i = suffixStartIndex + 1; i < input.length; i++){
+            if(input[i] <= minElem){
+                minSuffixIndex = i;
+                minElem = input[i];
+            }
+        }
+        return minSuffixIndex;
+    }
+    
+    static void swap_min_suffix_last_elem_prefix(int suffixStartIndex, int minSuffixIndex, char[] input){
+        int lastPrefixIndex = suffixStartIndex - 1;
+        char lastPrefixElem = input[lastPrefixIndex];
+        char minPrefixElem = input[minSuffixIndex];
+        input[lastPrefixIndex] = minPrefixElem;
+        input[minSuffixIndex] = lastPrefixElem;
+    }
+    
+    static void reverseSuffix(int suffixStartIndex, char[] input){
+        int size = input.length;
+        int lastIndex = size - 1;
+        int middle = (size - suffixStartIndex) / 2;
+        for(int i = 0; i < middle; i++){
+            char temp;
+            temp = input[suffixStartIndex + i];
+            input[suffixStartIndex+i] = input[lastIndex-i];
+            input[lastIndex-i] = temp;
+        }
     }
 }
