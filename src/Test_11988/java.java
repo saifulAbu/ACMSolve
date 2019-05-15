becc33bd50e55c8c38a22f935cdd222d4f0a8fbf
siddartha saif <saifulabu25@gmail.com>
2019-05-15 00:11:45 -0500

    adding more stuffs

--- src/Test_11988/java.java
+++ src/Test_11988/java.java
@@ -5,14 +5,43 @@
  */
 package Test_11988;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author sid
  */
 public class java {
   
-  //keep an inde, where to insert to the list
+  //keep an index, where to insert to the list
   //whent you get [, reset the insert pointer to 0
   //when you get ], reset the insert pointer to list.size()
   //for everything else, insert the character at pointer, increment the pointer
+  int getIndex(char currentChar, int curIndex, int listSize){
+    return 0;
+  }
+  
+  public static void main(String args[]){
+    testLL();
+  }
+  
+  static void testLL(){
+    List l = new LinkedList<Integer>();
+    l.add(0, 0);
+    l.add(1, 1);
+    l.add(2, 3);
+    l.add(2, 2);
+    l.add(0, -1);
+    pLL(l);
+  }
+  
+  static void pLL(List<Integer> l){
+    String s = "";
+    for(int i : l){
+      s = s + " " + i;
+    }
+    System.out.println(s);
+  }
+  
 }
